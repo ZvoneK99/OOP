3a88602412dc78bf9e614b6a4b3f06b58d49c4de
Zvone <zvone.kozull1999@gmail.com>
2024-11-19 12:14:06 +0100

    Računanje staža zaposlenika

--- Inheritance/src/zadatak09/Test.java
+++ Inheritance/src/zadatak09/Test.java
@@ -8,18 +8,19 @@ public class Test {
 		ArrayList<Zaposlenik> zaposlenici = new ArrayList<>();
 		Scanner ulaz = new Scanner(System.in);
 		
-		zaposlenici.add(new Zaposlenik("Ivan", "Ivić", 1985, "Programer", 3000.0));
-		zaposlenici.add(new Zaposlenik("Ana", "Anić", 1990, "Računovođa", 2500.0));
-		zaposlenici.add(new Zaposlenik("Marko", "Marić", 1980, "Voditelj", 4500.0));
-		zaposlenici.add(new Zaposlenik("Lucija", "Lucić", 1995, "Dizajner", 3200.0));
-		zaposlenici.add(new Zaposlenik("Petar", "Perić", 1992, "Sistem administrator", 2700.0));
-		zaposlenici.add(new Zaposlenik("Mia", "Matić", 1988, "Marketing", 2800.0));
-		zaposlenici.add(new Zaposlenik("Sara", "Sarić", 1996, "Programer", 3100.0));
-		zaposlenici.add(new Zaposlenik("Luka", "Lukić", 1983, "Voditelj", 4000.0));
-		zaposlenici.add(new Zaposlenik("Tina", "Tinić", 1991, "Računovođa", 2600.0));
-		zaposlenici.add(new Zaposlenik("Josip", "Josić", 1987, "Prodavač", 2900.0));
+		zaposlenici.add(new Zaposlenik("Ivan", "Ivić", 1985, "Programer", 3000.0, 2000));
+		zaposlenici.add(new Zaposlenik("Ana", "Anić", 1990, "Računovođa", 2500.0, 2005));
+		zaposlenici.add(new Zaposlenik("Marko", "Marić", 1980, "Voditelj", 4500.0, 2010));
+		zaposlenici.add(new Zaposlenik("Lucija", "Lucić", 1995, "Dizajner", 3200.0, 2012));
+		zaposlenici.add(new Zaposlenik("Petar", "Perić", 1992, "Sistem administrator", 2700.0, 2020));
+		zaposlenici.add(new Zaposlenik("Mia", "Matić", 1988, "Marketing", 2800.0, 2017));
+		zaposlenici.add(new Zaposlenik("Sara", "Sarić", 1996, "Programer", 3100.0, 2019));
+		zaposlenici.add(new Zaposlenik("Luka", "Lukić", 1983, "Voditelj", 4000.0, 2024));
+		zaposlenici.add(new Zaposlenik("Tina", "Tinić", 1991, "Računovođa", 2600.0, 2020));
+		zaposlenici.add(new Zaposlenik("Josip", "Josić", 1987, "Prodavač", 2900.0, 2009));
 		
-		System.out.println("Unesite prag da provjerimo osobe koje imaju plaću veću od njega.");
+		//Provjera koji zaposlenici imaju veću plaću od proslijeđene
+		/*System.out.println("Unesite prag da provjerimo osobe koje imaju plaću veću od njega.");
 		double prag = ulaz.nextDouble();
 		System.out.println("Zaposlenici sa plaćom većom od "+prag+" su:");
 		for(Zaposlenik z : zaposlenici) {
@@ -27,6 +28,17 @@ public class Test {
 				z.ispisiImePrezime();
 			}
 		}
+		*/
+		//Računanje staža zaposlenika
+		System.out.println("Unesite minimalni broj godina staža da ispišemo osobe sa više godina radnog staža.");
+		int stazZaposlenika = ulaz.nextInt();
+		System.out.println("Osobe sa vise od "+stazZaposlenika+" godina radnog staza su:");
+		for(Zaposlenik z : zaposlenici) {
+			if(z.izracunajStaz()> stazZaposlenika) {
+				z.ispisiImePrezime();
+			}
+		}
+		
 		ulaz.close();
 	}
 }
--- Inheritance/src/zadatak09/Zaposlenik.java
+++ Inheritance/src/zadatak09/Zaposlenik.java
@@ -1,16 +1,24 @@
 package zadatak09;
 
+import java.util.Calendar;
+
 public class Zaposlenik extends Osoba {
 	String pozicija;
 	double plaća;
-	
-	public Zaposlenik(String ime, String prezime, int godinaRođenja, String pozicija, double plaća) {
+	int godinaZaposlenja;
+	public Zaposlenik(String ime, String prezime, int godinaRođenja, String pozicija, double plaća, int godinaZaposlenja) {
 		super(ime, prezime, godinaRođenja);
 		this.pozicija = pozicija;
 		this.plaća = plaća;
+		this.godinaZaposlenja = godinaZaposlenja;
 	}
 	public boolean jeLiVisokPlaceni(double prag) {
 		return plaća>prag;
+	}
+	public int izracunajStaz() {
+		int trenutnaGodina = Calendar.getInstance().get(Calendar.YEAR);
+		int staz = trenutnaGodina-godinaZaposlenja;
+		return staz;
 	}
 	@Override
 	public void ispisiDetalje() {
