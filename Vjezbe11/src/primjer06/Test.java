8a840895d8babcad280bd223577770214efdcbbd
Zvone <zvone.kozull1999@gmail.com>
2024-11-25 10:38:47 +0100

    Polimorfizam i ispis broja razlicitih datoteka

--- Vjezbe11/src/primjer06/Test.java
+++ Vjezbe11/src/primjer06/Test.java
@@ -1,7 +1,11 @@
 package primjer06;
 
+import java.util.ArrayList;
+
 public class Test {
 	public static void main(String[]args) {
+		ArrayList<MultimedijskaDatoteka> datoteke = new ArrayList<>();
+		
 		MultimedijskaDatoteka m1 = new MultimedijskaDatoteka();
 		MultimedijskaDatoteka m2 = new MultimedijskaDatoteka("File.mp4", 700);
 		
@@ -11,15 +15,32 @@ public class Test {
 		AudioDatoteka a1 = new AudioDatoteka();
 		AudioDatoteka a2 = new AudioDatoteka("Pjesma.mp3", 5, 320, 3.5);
 		
-		m1.ispisiDetalje();
-		m2.ispisiDetalje();
-		System.out.println("--------------------------------");
-		v1.ispisiDetalje();
-		v2.ispisiDetalje();
-		System.out.println("--------------------------------");
-		a1.ispisiDetalje();
-		a2.ispisiDetalje();
-		System.out.println("--------------------------------");
+		datoteke.add(m1);
+		datoteke.add(m2);
+		datoteke.add(v1);
+		datoteke.add(v2);
+		datoteke.add(a1);
+		datoteke.add(a2);
+		
+		for(MultimedijskaDatoteka d : datoteke) {
+			d.ispisiDetalje();
+			System.out.println("--------------------------------");
+		}
+		
+		int brojVideoDatoteka = 0;
+        int brojAudioDatoteka = 0;
+		for(MultimedijskaDatoteka datoteka : datoteke) {
+			if(datoteka instanceof AudioDatoteka) {
+				brojAudioDatoteka++;
+			}
+		}
+		for(MultimedijskaDatoteka datoteka : datoteke) {
+			if(datoteka instanceof VideoDatoteka) {
+				brojVideoDatoteka++;
+			}
+		}
+		System.out.println("Broj Audio Datoteka: "+brojAudioDatoteka);
+		System.out.println("Broj Video Datoteka: "+brojVideoDatoteka);
 		System.out.println("Ukupan broj kreiranih datoteka: "+MultimedijskaDatoteka.brojKreiranihDatoteka());
 	}
 }
