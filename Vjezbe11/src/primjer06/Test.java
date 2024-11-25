ff5b6d014cb5d57b7811b81f1795c7127f784762
Zvone <zvone.kozull1999@gmail.com>
2024-11-25 10:22:51 +0100

    Dodane nove klase i metode u zadatku

--- Vjezbe11/src/primjer06/Test.java
+++ Vjezbe11/src/primjer06/Test.java
@@ -4,11 +4,22 @@ public class Test {
 	public static void main(String[]args) {
 		MultimedijskaDatoteka m1 = new MultimedijskaDatoteka();
 		MultimedijskaDatoteka m2 = new MultimedijskaDatoteka("File.mp4", 700);
-		MultimedijskaDatoteka m3 = new MultimedijskaDatoteka();
-		m1.otvori();
+		
+		VideoDatoteka v1 = new VideoDatoteka();
+		VideoDatoteka v2 = new VideoDatoteka("Film.mp4", 1500, "3840x2160", 120);
+		
+		AudioDatoteka a1 = new AudioDatoteka();
+		AudioDatoteka a2 = new AudioDatoteka("Pjesma.mp3", 5, 320, 3.5);
+		
 		m1.ispisiDetalje();
 		m2.ispisiDetalje();
-		m3.ispisiDetalje();
+		System.out.println("--------------------------------");
+		v1.ispisiDetalje();
+		v2.ispisiDetalje();
+		System.out.println("--------------------------------");
+		a1.ispisiDetalje();
+		a2.ispisiDetalje();
+		System.out.println("--------------------------------");
 		System.out.println("Ukupan broj kreiranih datoteka: "+MultimedijskaDatoteka.brojKreiranihDatoteka());
 	}
 }
